92282d4f342bea7c3db104bcad796cc2aaf9227b
Billah, Tashrif <tbillah@bwh.harvard.edu>
2022-03-02 15:48:00 -0500

    functional site and date filters

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 
 import base64, io
 import dash
-from dash import dcc, html, dash_table
+from dash import dcc, html, dash_table, Dash, callback_context
 from dash.dependencies import Input, Output
 from dash.exceptions import PreventUpdate
 import dash_bootstrap_components as dbc
@@ -10,6 +10,7 @@ import dash_bootstrap_components as dbc
 from os.path import isfile, isdir, abspath, join as pjoin, dirname, splitext, basename
 from os import makedirs, getenv, remove, listdir
 
+import re
 import pandas as pd
 import numpy as np
 import logging
@@ -26,7 +27,7 @@ if not ROOTDIR:
     exit(1)
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css',dbc.themes.BOOTSTRAP,'styles.css']
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets, suppress_callback_exceptions=True, title='EEG Qc Tool', assets_folder=ROOTDIR, assets_url_path="/")
+app = Dash(__name__, external_stylesheets=external_stylesheets, suppress_callback_exceptions=True, title='EEG Qc Tool', assets_folder=ROOTDIR, assets_url_path="/")
 log= logging.getLogger('werkzeug')
 log.setLevel(logging.ERROR)
 
@@ -63,7 +64,7 @@ app.layout= html.Div(
             dbc.Col(html.Div(dcc.Input(id='tech',placeholder='technician'))),
             
             # filter button
-            dbc.Col(html.Button('Filter', id='date-filter', n_clicks=0))
+            dbc.Col(html.Button('Filter', id='global-filter', n_clicks=0))
         ]),
         
         html.Br(),
@@ -81,54 +82,85 @@ app.layout= html.Div(
 
 
 @app.callback(Output('table','children'),
-    [Input('start','value'), Input('end','value'), Input('date-filter', 'n_clicks')])
-def render_table(start, end, click):
+    [Input('start','value'), Input('end','value'),
+    Input('site','value'),
+    Input('global-filter', 'n_clicks')])
+def render_table(start, end, site, click):
+
+    changed = [p['prop_id'] for p in callback_context.triggered][0]
+    if 'global-filter' not in changed:
+        raise PreventUpdate
 
     dirs= glob(ROOTDIR+'/**/Figures', recursive=True)
 
-    headers= ['Subject','Session']+ suffixes[3:]
+
+    # filter by date
+    if start and end:
+        start=int(start.replace('/',''))
+        end=int(end.replace('/',''))
+        
+        dirs2=[]
+        for d in dirs:
+            ses= int(re.search('ses-(.+?)/', d).group(1))
+            if ses>=start and ses<=end:
+                dirs2.append(d)
+        
+        dirs= dirs2.copy()
+
+
+    # filter by site
+    if site:
+        # ./PHOENIX/PROTECTED/PronetLA/processed/LA00012/eeg/ses-20211118/Figures
+        site= '/'+site 
+        dirs= [d for d in dirs if site in d]
+
+
+    # filter by columns
+    '''
+    imgs=[]
+    if qcimg:
+        for d in dirs:
+            for q in qcimg:
+                imgs.append(glob(f'{d}/*{q}.png'))
+
+    else:
+        for d in dirs:
+            imgs.append(glob(f'{d}/*_[!QC].png'))
+            
+    imgs2= [img for img in list1 for list1 in imgs]
+    '''
+
+    # filter by QC score
+
+
+    # filter by technician
+
+
+    # callback for save and last-save
+
+    print(dirs)
+    headers= ['Subject','Session']+ suffixes
     rows= [dbc.Row([dbc.Col(html.Div(h)) for h in headers])]
     for i,d in enumerate(dirs):
         parts= d.split('/')
         sub= parts[-4]
         ses= parts[-2].split('-')[1]
-        imgs= glob(f'{d}/*_QC*png')
-        imgs= [img for img in imgs if not img.endswith('_QC.png')]
+        imgs= glob(f'{d}/*[!QC].png')
         
+        print(imgs)  
+         
         rows.append(
             dbc.Row(
                 [dbc.Col(html.Div(sub)), dbc.Col(html.Div(ses))]+ \
-                [dbc.Col(html.Img(src=img.replace(ROOTDIR,''))) for img in imgs[3:]]
+                [dbc.Col(html.Img(src=img.replace(ROOTDIR,''))) for img in imgs]
             )
         )
-
-
-    if click and start and end:
-        start=""
-        end=""
-        
-        # filter the data
 
     
     return rows
 
 
-# filter by date
 
-
-# filter by site
-
-
-# filter by columns
-
-
-# filter by QC score
-
-
-# filter by technician
-
-
-# callback for save and last-save
 
 
 if __name__=='__main__':
